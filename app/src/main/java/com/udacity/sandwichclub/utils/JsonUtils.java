55ed4ff11e62bf74da97d3f961b60feb2a3eb057
Daniele Orlando <daniele.orlando@gmail.com>
2018-02-19 11:20:36 +0100

    fix with tips from reviewer

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,26 +11,34 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static final String JSON_SANDWICH_NAME = "name";
+    public static final String JSON_SANDWICH_MAINNAME = "mainName";
+    public static final String JSON_SANDWICH_PLACE = "placeOfOrigin";
+    public static final String JSON_SANDWICH_DESCRIPTION = "description";
+    public static final String JSON_SANDWICH_IMAGE = "image";
+    public static final String JSON_SANDWICH_AKA = "alsoKnownAs";
+    public static final String JSON_SANDWICH_INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
 
         try {
             Sandwich sandwich = new Sandwich();
             JSONObject jsonObject = new JSONObject(json);
 
-            sandwich.setMainName(jsonObject.getJSONObject("name").getString("mainName"));
-            sandwich.setPlaceOfOrigin(jsonObject.getString("placeOfOrigin"));
-            sandwich.setDescription(jsonObject.getString("description"));
-            sandwich.setImage(jsonObject.getString("image"));
+            sandwich.setMainName(jsonObject.getJSONObject(JSON_SANDWICH_NAME).getString(JSON_SANDWICH_MAINNAME));
+            sandwich.setPlaceOfOrigin(jsonObject.getString(JSON_SANDWICH_PLACE));
+            sandwich.setDescription(jsonObject.getString(JSON_SANDWICH_DESCRIPTION));
+            sandwich.setImage(jsonObject.getString(JSON_SANDWICH_IMAGE));
 
             List<String> listAKA = new ArrayList<>();
-            JSONArray jsonArrayAKA = jsonObject.getJSONObject("name").getJSONArray("alsoKnownAs");
+            JSONArray jsonArrayAKA = jsonObject.getJSONObject(JSON_SANDWICH_NAME).getJSONArray(JSON_SANDWICH_AKA);
             for (int i=0; i<jsonArrayAKA.length(); i++) {
                 listAKA.add(jsonArrayAKA.get(i).toString());
             }
             sandwich.setAlsoKnownAs(listAKA);
 
             List<String> listIngredients = new ArrayList<>();
-            JSONArray jsonArrayIngredients = jsonObject.getJSONArray("ingredients");
+            JSONArray jsonArrayIngredients = jsonObject.getJSONArray(JSON_SANDWICH_INGREDIENTS);
             for (int i=0; i<jsonArrayIngredients.length(); i++) {
                 listIngredients.add(jsonArrayIngredients.get(i).toString());
             }
